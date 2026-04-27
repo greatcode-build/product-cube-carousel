506885d15028723ef47bab55ae456e97c8ba91a4
Great Lucky <lgreat098@gmail.com>
2026-04-28 00:14:25 +0100

    cube gallery

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+});
